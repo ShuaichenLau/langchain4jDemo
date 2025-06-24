0d9c7d1bd8be9b126e5edecfcb0f0bb6f7dbe6f7
liusc <68754807@qq.com>
2025-06-24 22:54:58 +0800

    add:阿里通义百炼平台 文生图 demo 编写6

--- langchain4jDemo/src/test/java/com/yuan/QianWenTest.java
+++ langchain4jDemo/src/test/java/com/yuan/QianWenTest.java
@@ -2,12 +2,17 @@ package com.yuan;
 
 
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.community.model.dashscope.WanxImageModel;
+import dev.langchain4j.data.image.Image;
 import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
 
 /**
  * deepseek-r1 demo 测试
@@ -37,5 +42,20 @@ public class QianWenTest {
         logger.info(chat);
     }
 
+
+    @Test
+    public void testDashScopeWanx() {
+        WanxImageModel wanxImageModel = WanxImageModel.builder().modelName("wanx2.1-t2i-turbo")
+                .apiKey("sk-0ee8c7f6e1034cbe9f948fe80586146e1").build();
+
+        Response<Image> imageResponse = wanxImageModel.generate("奇幻森林精灵：在一片弥漫着轻柔薄雾的" +
+                "古老森林深处，阳光透过茂密枝叶洒下金色光斑。一位身材娇小、长着透明薄翼的精灵少女站在一朵硕大的蘑菇上。她" +
+                "有着海藻般的绿色长发，发间点缀着蓝色的小花，皮肤泛着珍珠般的微光。身上穿着由翠绿树叶和白色藤蔓编织而成的" +
+                "连衣裙，手中捧着一颗散发着柔和光芒的水晶球，周围环绕着五彩斑斓的蝴蝶，脚下是铺满苔藓的地面，蘑菇和蕨类植" +
+                "物丛生，营造出神秘而梦幻的氛围。");
+        logger.info("{}", imageResponse.content().url());
+
+
+    }
 
 }
