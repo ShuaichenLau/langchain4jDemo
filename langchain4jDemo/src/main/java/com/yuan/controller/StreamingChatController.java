ad5c1c7e49fa46645b3616e689808d48fd63b84f
liusc <68754807@qq.com>
2025-08-10 20:35:27 +0800

    update: 流式输出

--- langchain4jDemo/src/main/java/com/yuan/controller/StreamingChatController.java
+++ langchain4jDemo/src/main/java/com/yuan/controller/StreamingChatController.java
@@ -17,6 +17,12 @@ public class StreamingChatController {
     @Autowired
     private OpenAiStreamingChatModel model;
 
+    /**
+     * 验证流式输出回答
+     * http://localhost:38080/stream-chat?message=%E4%BD%A0%E5%A5%BD%EF%BC%8C%E8%AF%B7%E4%BB%8B%E7%BB%8D%E4%B8%80%E4%B8%8B%E4%BD%A0%E8%87%AA%E5%B7%B1
+     * @param message
+     * @return
+     */
     @GetMapping(value = "/stream-chat", produces = "text/stream;charset=UTF-8")
     public Flux<String> streamChat(@RequestParam("message") String message) {
         return Flux.create(sink -> {
